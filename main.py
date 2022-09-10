2ad6b675bc8cf1245805f9e305197bfec36825d4
Craig McIlwrath <craigmc08@gmail.com>
2022-09-10 15:32:33 -0400

    Add page for viewing requests (temporary)

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ import sqlite3
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SECRET_KEY'] = config.web.secret_key
 
-conn = sqlite3.connect(config.db.db_file)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = AccountForm()
     if form.validate_on_submit():
-        create_request(conn, form)
+        with sqlite3.connect(config.db.db_file) as conn:
+            create_request(conn, form)
         return redirect('/success')
     return render_template('index.html', form=form)
 
@@ -21,6 +20,14 @@ def index():
 @app.route('/success')
 def success():
     return render_template('success.html')
+
+
+@app.route('/requests')
+def requests():
+    with sqlite3.connect(config.db.db_file) as conn:
+        cur = conn.execute('SELECT * FROM active_requests')
+        requests = cur.fetchall()
+    return render_template('requests.html', requests=requests)
 
 
 if __name__ == '__main__':
